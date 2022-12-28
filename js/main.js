30d0987c739ba575739281c1bff381318c995f18
cardevv <113392365+cardevv@users.noreply.github.com>
2022-12-28 18:07:01 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
+// Operador lógico que retorna com dados salvos, ou string vazia, utilizando localStorage.getItem, modificando o valor de `string` com JSON.parse()
+
 const form = document.getElementById("novoitem")
 const lista = document.getElementById("lista")
 const itens = JSON.parse(localStorage.getItem("itens")) || []
 
 
 console.log(itens)
-
+// Uso do forEach para que todos os itens já escritos na lista sejam mantidos ao atualizar a página 
  itens.forEach(elemento => {
   criaElemento(elemento)
   
  });
 
-
+// Refatoração do addEventListener para receber as funções extras da função criaElemento
 form.addEventListener("submit", function (evento) {
   evento.preventDefault();
 
   const nome =evento.target.elements['nome'];
   const quantidade =evento.target.elements['quantidade'];
+
+  const existe = itens.find(elemento => elemento.nome === nome.value)
+
+  if (existe) {
+
+    quantidade.value += quantidade.value;
+
+  }
 
 
   const itemAtual = {
@@ -41,7 +51,7 @@ localStorage.setItem("itens", JSON.stringify(itens))
 
 
 })
-
+ // Refatoração da função `criaElemento` para que possua apenas a função que faça sentido ao nome.
 function criaElemento (item) {
 
     const novoItem = document.createElement('li');
